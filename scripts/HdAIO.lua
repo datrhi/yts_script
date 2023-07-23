aa1602d41acb07df20f8f3cd12581a08f147f422
Hieu Dang <hieu.dang@mservice.com.vn>
2023-07-23 11:00:59 +0700

    refactor: Restructure

---
+++ scripts/HdAIO.lua
@@ -0,0 +1,321 @@
+ChampName = game.localPlayer:ChampionName()
+
+if ChampName == "Aatrox" then
+	require("scripts/HDAIO/aatrox")
+elseif ChampName == "Ahri" then
+	require("scripts/HDAIO/ahri")
+elseif ChampName == "Akali" then
+	require("scripts/HDAIO/akali")
+elseif ChampName == "Alistar" then
+	require("scripts/HDAIO/alistar")
+elseif ChampName == "Amumu" then
+	require("scripts/HDAIO/amumu")
+elseif ChampName == "Anivia" then
+	require("scripts/HDAIO/anivia")
+elseif ChampName == "Annie" then
+	require("scripts/HDAIO/annie")
+elseif ChampName == "Aphelios" then
+	require("scripts/HDAIO/aphelios")
+elseif ChampName == "Ashe" then
+	require("scripts/HDAIO/ashe")
+elseif ChampName == "AurelionSol" then
+	require("scripts/HDAIO/aurelionsol")
+elseif ChampName == "Azir" then
+	require("scripts/HDAIO/azir")
+elseif ChampName == "Bard" then
+	require("scripts/HDAIO/bard")
+elseif ChampName == "Belveth" then
+	require("scripts/HDAIO/belveth")
+elseif ChampName == "Blitzcrank" then
+	require("scripts/HDAIO/blitzcrank")
+elseif ChampName == "Brand" then
+	require("scripts/HDAIO/brand")
+elseif ChampName == "Braum" then
+	require("scripts/HDAIO/braum")
+elseif ChampName == "Caitlyn" then
+	require("scripts/HDAIO/caitlyn")
+elseif ChampName == "Camille" then
+	require("scripts/HDAIO/camille")
+elseif ChampName == "Cassiopeia" then
+	require("scripts/HDAIO/cassiopeia")
+elseif ChampName == "Chogath" then
+	require("scripts/HDAIO/chogath")
+elseif ChampName == "Corki" then
+	require("scripts/HDAIO/corki")
+elseif ChampName == "Darius" then
+	require("scripts/HDAIO/darius")
+elseif ChampName == "Diana" then
+	require("scripts/HDAIO/diana")
+elseif ChampName == "Draven" then
+	require("scripts/HDAIO/draven")
+elseif ChampName == "DrMundo" then
+	require("scripts/HDAIO/drmundo")
+elseif ChampName == "Ekko" then
+	require("scripts/HDAIO/ekko")
+elseif ChampName == "Elise" then
+	require("scripts/HDAIO/elise")
+elseif ChampName == "Evelynn" then
+	require("scripts/HDAIO/evelynn")
+elseif ChampName == "Ezreal" then
+	require("scripts/HDAIO/ezreal")
+elseif ChampName == "Fiddlesticks" then
+	require("scripts/HDAIO/fiddlesticks")
+elseif ChampName == "Fiora" then
+	require("scripts/HDAIO/fiora")
+elseif ChampName == "Fizz" then
+	require("scripts/HDAIO/fizz")
+elseif ChampName == "Galio" then
+	require("scripts/HDAIO/galio")
+elseif ChampName == "Gangplank" then
+	require("scripts/HDAIO/gangplank")
+elseif ChampName == "Garen" then
+	require("scripts/HDAIO/garen")
+elseif ChampName == "Gnar" then
+	require("scripts/HDAIO/gnar")
+elseif ChampName == "Gragas" then
+	require("scripts/HDAIO/gragas")
+elseif ChampName == "Graves" then
+	require("scripts/HDAIO/graves")
+elseif ChampName == "Gwen" then
+	require("scripts/HDAIO/gwen")
+elseif ChampName == "Hecarim" then
+	require("scripts/HDAIO/hecarim")
+elseif ChampName == "Heimerdinger" then
+	require("scripts/HDAIO/heimerdinger")
+elseif ChampName == "Illaoi" then
+	require("scripts/HDAIO/illaoi")
+elseif ChampName == "Irelia" then
+	require("scripts/HDAIO/irelia")
+elseif ChampName == "Ivern" then
+	require("scripts/HDAIO/ivern")
+elseif ChampName == "Janna" then
+	require("scripts/HDAIO/janna")
+elseif ChampName == "JarvanIV" then
+	require("scripts/HDAIO/jarvaniv")
+elseif ChampName == "Jax" then
+	require("scripts/HDAIO/jax")
+elseif ChampName == "Jayce" then
+	require("scripts/HDAIO/jayce")
+elseif ChampName == "Jhin" then
+	require("scripts/HDAIO/jhin")
+elseif ChampName == "Jinx" then
+	require("scripts/HDAIO/jinx")
+elseif ChampName == "Kaisa" then
+	require("scripts/HDAIO/kaisa")
+elseif ChampName == "Kalista" then
+	require("scripts/HDAIO/kalista")
+elseif ChampName == "Karma" then
+	require("scripts/HDAIO/karma")
+elseif ChampName == "Karthus" then
+	require("scripts/HDAIO/karthus")
+elseif ChampName == "Kassadin" then
+	require("scripts/HDAIO/kassadin")
+elseif ChampName == "Katarina" then
+	require("scripts/HDAIO/katarina")
+elseif ChampName == "Kayle" then
+	require("scripts/HDAIO/kayle")
+elseif ChampName == "Kayn" then
+	require("scripts/HDAIO/kayn")
+elseif ChampName == "Kennen" then
+	require("scripts/HDAIO/kennen")
+elseif ChampName == "Khazix" then
+	require("scripts/HDAIO/khazix")
+elseif ChampName == "Kindred" then
+	require("scripts/HDAIO/kindred")
+elseif ChampName == "Kled" then
+	require("scripts/HDAIO/kled")
+elseif ChampName == "KogMaw" then
+	require("scripts/HDAIO/kogmaw")
+elseif ChampName == "KSante" then
+	require("scripts/HDAIO/ksante")
+elseif ChampName == "Leblanc" then
+	require("scripts/HDAIO/leblanc")
+elseif ChampName == "LeeSin" then
+	require("scripts/HDAIO/leesin")
+elseif ChampName == "Leona" then
+	require("scripts/HDAIO/leona")
+elseif ChampName == "Lillia" then
+	require("scripts/HDAIO/lillia")
+elseif ChampName == "Lissandra" then
+	require("scripts/HDAIO/lissandra")
+elseif ChampName == "Lucian" then
+	require("scripts/HDAIO/lucian")
+elseif ChampName == "Lulu" then
+	require("scripts/HDAIO/lulu")
+elseif ChampName == "Lux" then
+	require("scripts/HDAIO/lux")
+elseif ChampName == "Malphite" then
+	require("scripts/HDAIO/malphite")
+elseif ChampName == "Malzahar" then
+	require("scripts/HDAIO/malzahar")
+elseif ChampName == "Maokai" then
+	require("scripts/HDAIO/maokai")
+elseif ChampName == "MasterYi" then
+	require("scripts/HDAIO/masteryi")
+elseif ChampName == "Milio" then
+	require("scripts/HDAIO/milio")	
+elseif ChampName == "MissFortune" then
+	require("scripts/HDAIO/missfortune")
+elseif ChampName == "Mordekaiser" then
+	require("scripts/HDAIO/mordekaiser")
+elseif ChampName == "Morgana" then
+	require("scripts/HDAIO/morgana")
+elseif ChampName == "Nami" then
+	require("scripts/HDAIO/nami")
+elseif ChampName == "Nasus" then
+	require("scripts/HDAIO/nasus")
+elseif ChampName == "Nautilus" then
+	require("scripts/HDAIO/nautilus")
+elseif ChampName == "Neeko" then
+	require("scripts/HDAIO/neeko")
+elseif ChampName == "Nidalee" then
+	require("scripts/HDAIO/nidalee")
+elseif ChampName == "Nilah" then
+	require("scripts/HDAIO/nilah")
+elseif ChampName == "Nocturne" then
+	require("scripts/HDAIO/nocturne")
+elseif ChampName == "Nunu" then
+	require("scripts/HDAIO/nunu")
+elseif ChampName == "Olaf" then
+	require("scripts/HDAIO/olaf")
+elseif ChampName == "Orianna" then
+	require("scripts/HDAIO/orianna")
+elseif ChampName == "Ornn" then
+	require("scripts/HDAIO/ornn")
+elseif ChampName == "Pantheon" then
+	require("scripts/HDAIO/pantheon")
+elseif ChampName == "Poppy" then
+	require("scripts/HDAIO/poppy")
+elseif ChampName == "Pyke" then
+	require("scripts/HDAIO/pyke")
+elseif ChampName == "Qiyana" then
+	require("scripts/HDAIO/qiyana")
+elseif ChampName == "Quinn" then
+	require("scripts/HDAIO/quinn")
+elseif ChampName == "Rakan" then
+	require("scripts/HDAIO/rakan")
+elseif ChampName == "Rammus" then
+	require("scripts/HDAIO/rammus")
+elseif ChampName == "RekSai" then
+	require("scripts/HDAIO/reksai")
+elseif ChampName == "Rell" then
+	require("scripts/HDAIO/rell")
+elseif ChampName == "Renekton" then
+	require("scripts/HDAIO/renekton")
+elseif ChampName == "Rengar" then
+	require("scripts/HDAIO/rengar")
+elseif ChampName == "Riven" then
+	require("scripts/HDAIO/riven")
+elseif ChampName == "Rumble" then
+	require("scripts/HDAIO/rumble")
+elseif ChampName == "Ryze" then
+	require("scripts/HDAIO/ryze")
+elseif ChampName == "Samira" then
+	require("scripts/HDAIO/samira")
+elseif ChampName == "Sejuani" then
+	require("scripts/HDAIO/sejuani")
+elseif ChampName == "Senna" then
+	require("scripts/HDAIO/senna")
+elseif ChampName == "Seraphine" then
+	require("scripts/HDAIO/seraphine")
+elseif ChampName == "Sett" then
+	require("scripts/HDAIO/sett")
+elseif ChampName == "Shaco" then
+	require("scripts/HDAIO/shaco")
+elseif ChampName == "Shen" then
+	require("scripts/HDAIO/shen")
+elseif ChampName == "Shyvana" then
+	require("scripts/HDAIO/shyvana")
+elseif ChampName == "Singed" then
+	require("scripts/HDAIO/singed")
+elseif ChampName == "Sion" then
+	require("scripts/HDAIO/sion")
+elseif ChampName == "Sivir" then
+	require("scripts/HDAIO/sivir")
+elseif ChampName == "Skarner" then
+	require("scripts/HDAIO/skarner")
+elseif ChampName == "Sona" then
+	require("scripts/HDAIO/sona")
+elseif ChampName == "Soraka" then
+	require("scripts/HDAIO/soraka")
+elseif ChampName == "Swain" then
+	require("scripts/HDAIO/swain")
+elseif ChampName == "Sylas" then
+	require("scripts/HDAIO/sylas")
+elseif ChampName == "Syndra" then
+	require("scripts/HDAIO/syndra")
+elseif ChampName == "TahmKench" then
+	require("scripts/HDAIO/tahmkench")
+elseif ChampName == "Taliyah" then
+	require("scripts/HDAIO/taliyah")
+elseif ChampName == "Talon" then
+	require("scripts/HDAIO/talon")
+elseif ChampName == "Taric" then
+	require("scripts/HDAIO/taric")
+elseif ChampName == "Teemo" then
+	require("scripts/HDAIO/teemo")
+elseif ChampName == "Thresh" then
+	require("scripts/HDAIO/thresh")
+elseif ChampName == "Tristana" then
+	require("scripts/HDAIO/tristana")
+elseif ChampName == "Trundle" then
+	require("scripts/HDAIO/trundle")
+elseif ChampName == "Tryndamere" then
+	require("scripts/HDAIO/tryndamere")
+elseif ChampName == "TwistedFate" then
+	require("scripts/HDAIO/twistedfate")
+elseif ChampName == "Twitch" then
+	require("scripts/HDAIO/twitch")
+elseif ChampName == "Udyr" then
+	require("scripts/HDAIO/udyr")
+elseif ChampName == "Urgot" then
+	require("scripts/HDAIO/urgot")
+elseif ChampName == "Varus" then
+	require("scripts/HDAIO/varus")
+elseif ChampName == "Vayne" then
+	require("scripts/HDAIO/vayne")
+elseif ChampName == "Veigar" then
+	require("scripts/HDAIO/veigar")
+elseif ChampName == "Velkoz" then
+	require("scripts/HDAIO/velkoz")
+elseif ChampName == "Vi" then
+	require("scripts/HDAIO/vi")
+elseif ChampName == "Viego" then
+	require("scripts/HDAIO/viego")
+elseif ChampName == "Viktor" then
+	require("scripts/HDAIO/viktor")
+elseif ChampName == "Vladimir" then
+	require("scripts/HDAIO/vladimir")
+elseif ChampName == "Volibear" then
+	require("scripts/HDAIO/volibear")
+elseif ChampName == "Warwick" then
+	require("scripts/HDAIO/warwick")
+elseif ChampName == "Wukong" then
+	require("scripts/HDAIO/wukong")
+elseif ChampName == "Xayah" then
+	require("scripts/HDAIO/xayah")
+elseif ChampName == "Xerath" then
+	require("scripts/HDAIO/xerath")
+elseif ChampName == "XinZhao" then
+	require("scripts/HDAIO/xinzhao")
+elseif ChampName == "Yasuo" then
+	require("scripts/HDAIO/yasuo")
+elseif ChampName == "Yone" then
+	require("scripts/HDAIO/yone")
+elseif ChampName == "Yorick" then
+	require("scripts/HDAIO/yorick")
+elseif ChampName == "Yuumi" then
+	require("scripts/HDAIO/yuumi")
+elseif ChampName == "Zac" then
+	require("scripts/HDAIO/zac")
+elseif ChampName == "Zed" then
+	require("scripts/HDAIO/zed")
+elseif ChampName == "Ziggs" then
+	require("scripts/HDAIO/ziggs")
+elseif ChampName == "Zilean" then
+	require("scripts/HDAIO/zilean")
+elseif ChampName == "Zoe" then
+	require("scripts/HDAIO/zoe")
+elseif ChampName == "Zyra" then
+	require("scripts/HDAIO/zyra")
+end
